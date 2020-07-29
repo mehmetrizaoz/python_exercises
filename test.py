6f47c05a2f4c8a8db7f9e532701098e7cb267929
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-07-29 14:34:54 +0300

    id was created randomly

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 import xml.etree.ElementTree as ET
 import sys, getopt
 from xml.dom import minidom
+from random import randint
 
 tree = ET.parse('items.xml')
 root = tree.getroot()
 lst = root.findall('no')
-wordId = sys.argv[1]
+random = randint(1, len(list(root)))
 
 for item in lst:
-   if (item.get('x') == wordId):
+   if (item.get('x') == str(random)):
       print(item.find('word').text + ": " + item.find('meaning').text + "\n")
       print(item.find('example').text)
-       
-   
+
+
+
+
