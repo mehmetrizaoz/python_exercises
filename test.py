2bcd447e2f2662b13287fdeb953601604791fb48
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-08-09 14:06:24 +0300

    infinite loop added

--- test.py
+++ test.py
@@ -6,14 +6,17 @@ from random import randint
 tree = ET.parse('items.xml')
 root = tree.getroot()
 lst = root.findall('no')
-random = randint(1, len(list(root)))
 
-for item in lst:
-   if (item.get('x') == str(random)):
-      print(item.find('word').text)
-      input()
-      print(item.find('meaning').text)
-      print(item.find('example').text)
+var = 1
+while var == 1:
+   random = randint(1, len(list(root)))
+   for item in lst:
+      if (item.get('x') == str(random)):
+         print(item.find('word').text)
+         input()
+         print(item.find('meaning').text)
+         print(item.find('example').text)
+         print("***********************")
 
 
 
